9048bb209d53032ce224d1f6bc133eaae33df8d3
IsdaPerez <89265606+IsdaPM@users.noreply.github.com>
2022-02-20 23:02:42 -0600

    Add n1

---
+++ mod 1 - program.py
@@ -0,0 +1,23 @@
+'''sum = 2 + 1
+print(sum)
+
+print("tu anime favorito")
+fav = input("¿cuál es?")
+print(fav, "es un buen anime:)")'''
+
+''''print("calculate")
+num1 = input("put a number ")
+num2 = input("put other number ")
+print(int(num1)+int(num2))'''
+
+
+parsec = 1
+
+lightyears = 3.26156
+
+print(str(parsec) + " parsec, is " + str(lightyears) + " lightyears")
+
+parsecs = input("ingresa la cantidad de parsecs: ")
+lightyear = int(parsecs)*lightyears
+print(parsecs, "parsecs es igual a", lightyear, "lightyears")
+
